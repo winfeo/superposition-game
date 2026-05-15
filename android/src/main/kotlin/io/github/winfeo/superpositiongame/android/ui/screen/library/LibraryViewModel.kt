4360fdefd3c4cdf987c0dddf4bdcb94ed7896dc0
winfeo <kirill.ryabikin.05@gmail.com>
2026-05-15 15:10:54 +0300

    feat(android): Справочник всех карт

--- android/src/main/kotlin/io/github/winfeo/superpositiongame/android/ui/screen/library/LibraryViewModel.kt
+++ android/src/main/kotlin/io/github/winfeo/superpositiongame/android/ui/screen/library/LibraryViewModel.kt
@@ -1,7 +1,28 @@
 package io.github.winfeo.superpositiongame.android.ui.screen.library
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.winfeo.superpositiongame.android.data.repository.CardsRepositoryImpl
+import io.github.winfeo.superpositiongame.android.domain.library.Card
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class LibraryViewModel(): ViewModel() {
+    private val repository = CardsRepositoryImpl()
 
+    private val _cards = MutableStateFlow<List<Card>>(emptyList())
+    val cards: StateFlow<List<Card>> = _cards
+
+    init {
+        loadAllCards()
+    }
+
+    private fun loadAllCards() {
+        viewModelScope.launch {
+            repository.getAllCards().collect { cards ->
+                _cards.value = cards
+            }
+        }
+    }
 }
